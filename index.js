a5e9e7209e5ef3ee15a54f6f00984f360302fa3f
Jonas Schmid <jonas.schmid@gmail.com>
2014-04-18 19:26:38 +0200

    Fetch all cells

--- index.js
+++ index.js
@@ -3,14 +3,56 @@ var Client = require('node-rest-client').Client;
 
 var client = new Client();
 
+var allCells = new Array();
+var found = new Array();
+
 var processCell = function (cell, callback) {
-    console.log('hello ' + cell);
-    callback();
+    console.log('Processing cell: ' + cell.id + ' [' + cell.row + ',' + cell.column + ']');
+    client.get('http://apibunny.com/cells/' + cell.id, fetchCell(cell, callback));
 };
 
 var q = async.queue(processCell, 1);
 
-client.get('http://apibunny.com/mazes', function(data, response) {
-  obj = JSON.parse(data);
-  console.log(obj.mazes[0].links.start);
-});
+q.drain = function() {
+    console.log('All items have been processed');
+}
+
+var fetchMaze = function(data, response) {
+  var obj = JSON.parse(data);
+  var startCell = obj.mazes[0].links.start;
+  found.push(startCell);
+  q.push({
+    'id': startCell,
+    'row': 0,
+    'column' : 0
+  });
+};
+
+var fetchCell = function(cell, callback) {
+  return function(data, response) {
+    var obj = JSON.parse(data);
+    var links = obj.cells[0].links;
+    console.log(links);
+
+    checkCell(links, 'east', cell.row, cell.column + 1);
+    checkCell(links, 'west', cell.row, cell.column - 1);
+    checkCell(links, 'north', cell.row - 1, cell.column);
+    checkCell(links, 'south', cell.row + 1, cell.column);
+
+    callback();
+  }
+};
+
+var checkCell = function(links, direction, row, column) {
+  var newCell = links[direction];
+  if(newCell && found.indexOf(newCell) < 0) {
+    found.push(newCell);
+    q.push({
+      'id': newCell,
+      'row': row,
+      'column' : column
+    });
+  }
+}
+
+client.get('http://apibunny.com/mazes', fetchMaze);
